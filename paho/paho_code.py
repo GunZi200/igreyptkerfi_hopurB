65aea9066c8e96dcb5cd2bf55c36d8b5577f2676
Guðni Már Gilbert <gudni.m.g@gmail.com>
2020-04-05 07:58:10 +0000

    Bætti við kóða sem mælir söfnunartíðni

--- paho/paho_code.py
+++ paho/paho_code.py
@@ -1,5 +1,11 @@
 # %%
 import paho.mqtt.client as mqtt
+from time import time 
+import math
+
+T0 = T1 = T4 = None
+counter0 = None
+counter1 = None
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -13,11 +19,23 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("ADC1", 0)
     print("Done.")
     
-
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    global T1, T4, counter0, counter1
     #print(msg.topic+" "+str(msg.payload))
-    print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
+    
+    if msg.topic == "ADC0":
+        counter0 += 1
+        if counter0 % 1000 == 0:
+            T2 = time()
+            print('ADC0 receiving', math.ceil(1000/ (T2 - T1)), 'messages per sec')
+            T1 = T2
+    if msg.topic == "ADC1":
+        counter1 += 1
+        if counter1 % 1000 == 0:
+            T3 = time()
+            print('ADC1 receiving', math.ceil(1000/ (T3 - T4)), 'messages per sec')
+            T4 = T3
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -25,6 +43,10 @@ client.on_message = on_message
 
 #client.username_pw_set("username1", "pwd1")
 client.connect("mqtt.eclipse.org", 1883, 60)
+
+T0 = T1 = T4 = time()
+counter0 = 0
+counter1 = 0
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
